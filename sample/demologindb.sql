5f736f6a960876fca306265cd7caf796756a3c11
Tanvi <tk63@njit.edu>
2025-10-20 15:33:53 -0400

    updates to backend

--- sample/demologindb.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IT490;
-Create table users(id int not null auto_increment primary key, name varchar(255), username varchar(255), password varchar(255) not null);
-INSERT INTO users (name, username, password) VALUES (steve, steve12, IloveSQL);
-
