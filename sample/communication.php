56ed54eac52e7f23799137d76adeadce8cc682a3
Emily <esm33@njit.edu>
2025-10-21 00:10:45 -0400

    accidentally made edits in login.php when it should've been COMMUNICATION.phpgit add .

--- sample/communication.php
+++ sample/communication.php
@@ -1,5 +1,11 @@
 <?php
 
+//pings this if can't connect to the db
+if($conn -> errno != 0)
+{echo "failed to connect to database: ". $conn->error . PHP_EOL;
+ecit(0);
+//display this if it can connect to the db
+echo "successfully connected to the database";
 
 if (!isset($_POST))
 {
@@ -15,37 +21,29 @@ switch ($request["type"])
 		//get the username value and the password value
 		$usr = $request["uname"];
 		$pwd = $request["pword"];
+
 		//if statements to check for specific credentials 
 		if($usr == "kehoed" && $pwd == "12345")
 		{
 			//if the username value and password value math
 			//then set the response message to success
-			$response = "login, yeah we can do that";
+			$response = "success";
+			$_SESSION['successful_login'] = true;
+			$_SESSION['username_profile'] = $usr;
 		}
 		else 
 		{
 			//else, set the response message to fail
-			$response = "login failed, yeah we can't do that";
+			$response = "fail";
 		}
 	break;
 
 	case "registration":
-		//get the username value and the password value
-		$usr = $request["uname"];
-		$pwd = $request["pword"];
-		//if statements to check for specific credentials 
-		if($usr == "kehoed" && $pwd == "12345")
 		{
-			//if the username value and password value math
-			//then set the response message to success
-			$response = "login, yeah we can do that";
-		}
-		else 
-		{
-			//else, set the response message to fail
-			$response = "login failed, yeah we can't do that";
+	
 		}
 	break;
+
 }
 echo json_encode($response);
 exit(0);
