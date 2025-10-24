10952910550d6711097d89e12638a0411413ad45
Collin Jones <cmj09@live.com>
2025-10-24 02:26:07 -0400

    passing along current sql files and db files

--- sample/DBTEST/dblistener.php
+++ sample/DBTEST/dblistener.php
@@ -1,6 +1,7 @@
 <?php
 require_once('login.php.inc');
-
+//hi 
+//updated case
 // DB Listener for RabbitMQ
 $connection = new AMQPConnection([
     'host' => '100.86.240.90',
@@ -26,40 +27,8 @@ $exchange->declareExchange();
 $queue = new AMQPQueue($channel);
 $queue->setName('testQueue');
 $queue->setFlags(AMQP_DURABLE);
-$queue->setFlags(AMQP_DURABLE);
 $queue->declareQueue();
 $queue->bind('testExchange', 'db_route');
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$username = $_POST['username'];
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	
-	$checkEmailStmt = $connection->prepare("SELECT email FROM users WHERE email = ?");
-    	$checkEmailStmt->bind_param("s", $email);
-    	$checkEmailStmt->execute();
-    	$checkEmailStmt->store_result();
-    	
-	if ($checkEmailStmt->num_rows > 0) {
-        $message = "Email ID already exists";
-        $toastClass = "#007bff"; //blue reset color
-    } else {
-        $stmt = $connection->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $username, $email, $password);
-
-        if ($stmt->execute()) {
-            $message = "Account created successfully";
-            $toastClass = "#28a745"; //green success color
-        } else {
-            $message = "Error: " . $stmt->error;
-            $toastClass = "#dc3545"; // red error color
-        }
-
-        $stmt->close();
-    }
-     $checkEmailStmt->close();
-     $connection->close();
-}
 
 echo " [x] Waiting for messages on 'testQueue'...\n";
 
@@ -70,60 +39,55 @@ while (true) {
 
         $data = json_decode($body, true);
        $request = $_POST;
-    if(isset($payload['type'])){  
+    if(isset($data["type"])){  
+    echo "type received";
+   
     $loginDB = new loginDB();
     $connDB = $loginDB->getConnection();
-    switch ($payload['type']){
-    
-    
-    
-    
-
-	case "login":
+    switch ($data['type']){
+  	case "login":
 		$type = "login";
 		//get the username value and the password value
-		$usr = $request["uname"];
-		$pwd = $request["pword"];
-
-
-	$queryAllCategories = 'SELECT username,password FROM users
-                        WHERE username = :usr';
-$statement2 = $db->prepare($queryAllCategories);
-$statement2 ->bindValue(':emailAddress', $emailAddress);
-$statement2->execute();
-	$managers = $statement2->fetch();
-
-        if ( $statement2->execute()) {
-        echo "fetching your data is sucessful!" ; }
-        else {
-        echo "something went wrong please try again!";}
-
-		//if statements to check for specific credentials 
-		if($usr == $managers['username'] && $pwd == $managers['password'])
-		{
-			//if the username value and password value math
-			//then set the response message to success
-			$response = "success";
-			$_SESSION['successful_login'] = true;
-			$_SESSION['username_profile'] = $usr;
-		}
-		else 
-		{
-			//else, set the response message to fail
-			$response = "fail";
-		}
-	break;    
+		echo ("this is case login.");
+		$success = $loginDB->validateLogin($data['uname'], $data['pword']);
+		if ($success) { ["status"=>"success","message"=>"login.successful"]; }
+		else {["status"=>"fail","message"=>"login failed"];}
+	break;
+	case "registration":
+			$type = "registration";
+			echo ("this is case registration.");
+		echo $data["uname"];
+		echo $data["pword"];
+		$success = $loginDB->registerUser($data['uname'], $data['pword']);
+		echo $success;
+		if ($success) { ["status"=>"success","message"=>"registration successful"]; }
+		else {["status"=>"fail","message"=>"registration failed"];}
+		
+        break;
     
-  
-    }
     
     }
     
-
-        $queue->ack($message->getDeliveryTag());
-    });
+    $replyexchange = new AMQPExchange($queue->getChannel());
+    if($message->getReplyTo())
+	{
+		$replyexchange->publish(
+			json_encode($success),
+			$message->getReplyTo(),
+			AMQP_NOPARAM,
+			['correlation_id'=>$message->getCorrelationId()]
+			);
+		echo ("I have sent a reply.");
+		echo ($success);
+	}
     
-   } 
+    }
+    	$queue->ack($message->getDeliveryTag());
+});
+
+}
+ 
+    
+   
 
 ?>
-
