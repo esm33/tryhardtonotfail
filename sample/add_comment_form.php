42bc6d1dbf8541e8b17bf3462c43b661bbabb187
Emily <esm33@njit.edu>
2025-10-27 20:40:04 -0400

    fixing indentations

--- sample/add_comment_form.php
+++ sample/add_comment_form.php
@@ -4,28 +4,35 @@
 		session_start();
 	}
 	
-	if(!isset($_SESSION['successful_login']))
-		{
-			header("Location: login.php");
-			exit(0);
-		}
+
 		
 //insert code here to connect to the database & grab the API 
+
+//query that selects all the drink names from the db and stores them in an array to be accessed for creating dropdown menu
+
 ?>
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Discussion Forum</title>
-		<link rel="stylesheet" href="./style.css">
-	</head>
-	<?php include 'navigationbar.php';>
-	<body>
+<head>
+	<title>Discussion Forum</title>
+	<link rel="stylesheet" href="./style.css">
+</head>
+<body>
+	<?php include 'navigationbar.php'; ?>
 	<main>
-	<div class="container">
+		<div class="container">
 		<div class="glass-card">
 			<h1> Add Commentary</h1>
 			<form action="" method="post" id="add_comment_form">
+				<select class="comment_dropdown_drinks">
+					<?php foreach ($recipe as $r):?>
+					<option value="<?php echo htmlspecialchars($r); ?>">
+					<?php echo htmlspecialchars($r); ?>
+					</option>
+					<?php endforeach; ?>
+				<select>
+			
 				<label>Comment</label>
 				<input type="text" name="user_comment" required><br>
 	
@@ -37,17 +44,19 @@
 	</main>
 	
 	<section>
-		<?php if count($comments) == 0: ?>
+		<?php if (count($comments)) === 0: ?>
 			<div class="glass-card">
 			<h2>There are no comments yet.</h2>
 			</div>
 		<?php else: ?>
 			<div class="glass-card">
 				<?php foreach ($comments as $comment): ?>
-					<p><?php echo $comment->getComment(); ?></p>
+				<h1>From: </h1>
+				<h2>Drink: </h2>
+				<p>Comment:<br><?php echo $comment->getComment(); ?></p>
 				<?php endforeach; ?>
 			</div>
 		<?php endif; ?>
 	</section>
-	</body>
+</body>
 </html>
