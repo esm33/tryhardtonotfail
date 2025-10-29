c0bc543317ca38b42e827a34fa0fe68d051a7c56
Emily <esm33@njit.edu>
2025-10-28 22:09:09 -0400

    replicating registration.html for recipe form

--- sample/add_recipe_form.php
+++ sample/add_recipe_form.php
@@ -8,41 +8,109 @@
 		{
 			header("Location: login.php");
 			exit(0);
-		}
-		
+		}	
 //insert code here to connect to the database & grab the API 
 ?>
-
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Add Recipe</title>
 		<link rel="stylesheet" href="./style.css">
 	</head>
+	<body>
+		<script>
+
+		function HandleAddRecipeFormResponse(response)
+		{
+			var text = JSON.parse(response);
+		//	document.getElementById("textResponse").innerHTML = response+"<p>";	
+			document.getElementById("textResponse").innerHTML = "response: "+text+"<p>";
+			console.log("response:", text);
+		}
+
+		function SendAddRecipeFormRequest(recipe_name,drink_type,drink_ingredient, drink_instructions)
+		{
+			var request = new XMLHttpRequest();
+			request.open("POST","communication.php",true);
+			request.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+			request.onreadystatechange= function ()
+			{
+				
+				if ((this.readyState == 4)&&(this.status == 200))
+				{
+					HandleAddRecipeFormResponse(this.responseText);
+					console.log("SendAddRecipeFormrequest function ready state done.");
+				}		
+			}
+			request.send("type=new_recipe&rname="+recipe_name+"&dtype="+drink_type+"&d_ingredient="+drink_ingredient+"&d_instructions="+drink_instructions);
+			console.log("type new_recipe sent HERE");
+		}
+
+
+		function getRecipeInfo()
+		{
+			
+			const recipe_name_text_input = document.getElementById("recipe_name");
+			const recipe_name_input_value = recipe_name_text_input.value;
+
+			const drink_type_text_input = document.getElementById("drink_type");
+			const drink_type_input_value = drink_type_text_input.value;
+
+			const drink_ingredient_text_input = document.getElementById("drink_ingredient");
+			const drink_ingredient_input_value = drink_ingredient_text_input.value;
+
+			const drink_instructions_text_input = document.getElementById("drink_instructions");
+			const drink_instructions_input_value = drink_instructions_text_input.value;
+
+			
+			console.log("Recipe Name: ", recipe_name_input_value);
+			console.log("Drink Type: ", drink_type_input_value);
+			console.log("Drink Ingredient: ", drink_ingredient_input_value);
+			console.log("Drink Instructions: ", drink_instructions_input_value);
+
+			SendAddRecipeFormRequest(recipe_name_input_value, drink_type_input_value, drink_ingredient_input_value, drink_instructions_input_value);
+			console.log("sendAddRecipeFormrequest done");
+		}
+
+		</script>
+	</body>
+	
+	
 	<?php include 'navigationbar.php'; ?>
 	
-	<div>
+	<body>
 	<div class="container">
 		<div class="glass-card">
 			<h1> Add Recipe</h1>
-			<form action="" method="post" id="add_recipe_form">
-				<label>Drink Name</label>
-				<input type="text" name="drinkname" required><br>
+			<form id="add_recipe_form">
+				<div class="input-group">
+					<label for="recipe_name">Recipe Name: </label>
+					<input type="text" id="recipe_name" name="rname" placeholder="" required />
+				</div>
 				
-				<label>Drink Type</label>
-				<input type="text" name="drinktype" required><br>
+				<div class="input-group">
+					<label for="drink_type">Drink Type</label>
+					<input type="text" id="drink_type" name="dtype" placeholder="" required/><br>
+				</div>
 				
-				<label>Ingredient and Quantity</label>
-				<input type="text" name="drinkingredient" required><br>
-				
-				<label></label>
-				<input type="submit" value="Add Recipe"><br>
-			</form>
-			<p><a href="homecatalog.php">View Drink Catalog</a></p>
-		</div>
-	</div>
-	
-<body>
+				<div class="input-group">
+					<label for="drink_ingredient">Ingredient</label>
+					<input type="text" name="drink_ingredient" name="d_ingredient" placeholder="" required/><br>
+				</div>
+					
+				<div class="input-group">
+					<label for="drink_instructions">Instructions</label>
+					<input type="text" name="drink_instructions" name="d_instructions" placeholder="How do you prepare your drink?" required/><br>
+				</div>
 
-</body>
+				<button type="button" onclick="getRecipeInfo()" class="btn">Register</button>
+				
+			</form>
+			
+			<div class="login-link"> <!--DON'T FORGET TO ADD CSS FOR THIS LINK, MUST ADD CSS LINES FOR THIS LINK -->
+			<a href="homecatalog.php">View Drink Catalog</a>
+			</div>
+		</div>
+	</body>
+	
 </html>
