e377b294df9c7390d47f92696e525133dec93264
Tanvi <tk63@njit.edu>
2025-12-17 18:23:57 -0500

    search by drink name

--- APIcalls.php
+++ APIcalls.php
@@ -5,6 +5,11 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('communication.inc');
 
+def getPages(drink){
+	$results = shell_exec('GET www.thecocktaildb.com/api/json/v1/1/search.php?s={drink}');
+	$arrayCode = json_decode($results);
+	var_dump($arrayCode);
+}
 
 
 ?>
