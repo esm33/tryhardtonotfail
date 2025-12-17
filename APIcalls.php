8c512b4fb9fb6757ffa924c918a9041a2b1fc9b4
Tanvi <tk63@njit.edu>
2025-12-17 18:30:43 -0500

    getting a random drink. can be used for the homepage)

--- APIcalls.php
+++ APIcalls.php
@@ -5,11 +5,22 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('communication.inc');
 
-def getPages(drink){
-	$results = shell_exec('GET www.thecocktaildb.com/api/json/v1/1/search.php?s={drink}');
+def getRandom{
+//should show a random drink 
+	$results = shell_exec('GET  www.thecocktaildb.com/api/json/v1/1/random.php');
 	$arrayCode = json_decode($results);
 	var_dump($arrayCode);
 }
+
+def getPages(drink){
+//searching for a specific drink by name
+	$results = shell_exec('GET www.thecocktaildb.com/api/json/v1/1/search.php?s={drink}');
+	$arrayCode = json_decode($results);
+	var_dump($arrayCode);
+	
+}
+
+
 
 
 ?>
