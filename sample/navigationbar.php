588348eab5d989dbf0013b7bc5bd2b7fb60b4575
Emily <esm33@njit.edu>
2025-12-19 02:58:42 -0500

    adding new pages to navbar

--- sample/navigationbar.php
+++ sample/navigationbar.php
@@ -6,6 +6,8 @@
     <nav id="nav_menu">
         <ul>
             <li><a href="./homecatalog.php">Home Catalog</a></li>
+            <li><a href="./add_recipe_form.php">Add Recipe</a></li>
+            <li><a href="./add_rating_form.php">Add Rating</a></li>
             <li><a href="./profile.php">Profile</a></li>
             <li><a href="./logout.php">Log Out</a></li>
         </ul>
